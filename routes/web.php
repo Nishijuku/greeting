ee460be0092e8479074e6437657a70554345fca7
Wald-japan <shimoigusa.kobetsu@gmail.com>
2024-09-10 14:21:12 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/comments/morning', function(){
+    return view('message.morning');
+});
+
+Route::get('/comments/afternoon', function(){
+    return view('message.afternoon');
+});
+
+Route::get('/comments/evening', function(){
+    return view('message.evening');
+});
+
+Route::get('/comments/night', function(){
+    return view('message.night');
+});
+
+Route::get('/comments/freeword/{msg}', function($msg){
+    return view('message.freeword', ['msg'=> $msg]);
+});
+
